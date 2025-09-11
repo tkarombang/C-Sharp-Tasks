4064ac828cb0e6dac964c7fe3bf2197c75ed1113
tkarombang <tuangkarombang@gmail.com>
2025-09-11 18:02:49 +0800

    Fruits OOP

---
+++ 8-Fruits-oop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace _8_Fruits_oop
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+      Fruits Apel = new Fruits("Apel", "Merah");
+      Apel.Matang();
+
+    }
+  }
+
+  class Fruits(string nama, string warna)
+  {
+    private string Nama = nama;
+    private string Warna = warna;
+
+    public void Matang()
+    {
+      Console.WriteLine($"Buah {Nama} warnanya {Warna} menandakan sudah Matang");
+    }
+
+  }
+}
