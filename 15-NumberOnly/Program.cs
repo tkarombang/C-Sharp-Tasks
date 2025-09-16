d9ad74bd9ab8838db7a01c60b684ea6ff3ac4ae0
tkarombang <tuangkarombang@gmail.com>
2025-09-16 14:16:00 +0800

    Number Only

---
+++ 15-NumberOnly/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace _15_NumberOnly
+{
+  class Program
+  {
+    // public static bool IsNUmberOnly(string str)
+    public static string IsNUmberOnly(string str)
+    {
+      var angka = str.Where(c => char.IsDigit(c));
+      return new string(angka.ToArray());
+      // return str.All(c => char.IsDigit(c));
+    }
+    public static void Main(string[] args)
+    {
+      Console.WriteLine(IsNUmberOnly("12345"));
+      Console.WriteLine(IsNUmberOnly("12a45"));
+      Console.WriteLine(IsNUmberOnly("abcdase"));
+    }
+  }
+}
