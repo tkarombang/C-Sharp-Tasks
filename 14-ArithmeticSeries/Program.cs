9c76bbbf7b1e2fe58a826b3943a731c44106ba7d
tkarombang <tuangkarombang@gmail.com>
2025-09-16 14:07:50 +0800

    ArithmeticSeries 3

---
+++ 14-ArithmeticSeries/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace _14_ArithmeticSeries
+{
+  class Program
+  {
+    public static int HitungArithmeticSeries(int a, int d, int n)
+    {
+      return (n * (2 * a + (n - 1) * d)) / 2;
+    }
+
+    public static void CetakArithmeticSeries(int a, int d, int n)
+    {
+      for (int i = 0; i < n; i++)
+      {
+        int term = a + i * d;
+        Console.Write(term + " ");
+      }
+      Console.WriteLine();
+    }
+    public static void Main(string[] args)
+    {
+      int a = 2;
+      int d = 3;
+      int n = 5;
+
+      CetakArithmeticSeries(a, d, n);
+
+      int sum = HitungArithmeticSeries(a, d, n);
+      Console.WriteLine("Jumlah dari " + n + " Suku Pertama: " + sum); 
+
+    }
+  }
+
+}
