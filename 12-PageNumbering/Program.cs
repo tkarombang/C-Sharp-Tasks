1d3c4322d4c88adb541fe0f2099b5a1ff15013a0
tkarombang <tuangkarombang@gmail.com>
2025-09-16 13:18:47 +0800

    Page Numbering

---
+++ 12-PageNumbering/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace _12_PageNumbering
+{
+  class Program
+  {
+    public static int GetPageCount(int totalItems, int pageSize)
+    {
+      return (int)Math.Ceiling((double)totalItems / pageSize);
+    }
+    public static void Main(string[] args)
+    {
+      Console.WriteLine(GetPageCount(25, 10));
+      Console.WriteLine(GetPageCount(100, 20));
+
+      
+    }
+  }
+}
